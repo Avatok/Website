9e9562b374be60106763e90d1dee90483519eea4
Pascal <avatok.business@gmail.com>
2025-03-12 16:26:22 +0100

    Update code.js

--- JavaScript/code.js
+++ JavaScript/code.js
@@ -3,3 +3,20 @@ function ScrollDown(){
     element = document.getElementById("Q1");
     element.scrollIntoView();
 }
+
+//Typing effect Avatok - Pascal
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "Pascal - Avatok";
+    const target = document.getElementById("typing-effect");
+    let index = 0;
+
+    function typeEffect() {
+        if (index < text.length) {
+            target.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 150); // Geschwindigkeit anpassen
+        }
+    }
+
+    typeEffect();
+});
